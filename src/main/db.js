b66e53379b337c1812fe8f85e7ec655132c30c2d
pluckyguy-1 <124134641+pluckyguy-1@users.noreply.github.com>
2025-06-05 14:35:03 +0300

    Update db.js

--- src/main/db.js
+++ src/main/db.js
@@ -3,10 +3,10 @@ import { Client } from 'pg';
 export default async () => {
   const client = new Client({
     user: 'postgres',
-    password: '1234',
+    password: 'student123',
     host: 'localhost',
     port: '5432',
-    database: 'penis',
+    database: 'student2',
   });
 
   await client.connect();
